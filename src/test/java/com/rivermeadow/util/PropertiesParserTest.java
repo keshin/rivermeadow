fe44882bed1cebe8396cc09d17b3393438aa5d30
Ali Ghorbani Moghadam <amoghadam@ebay.com>
2014-01-08 22:02:59 -0800

    Adding more info

--- src/test/java/com/rivermeadow/util/PropertiesParserTest.java
+++ src/test/java/com/rivermeadow/util/PropertiesParserTest.java
@@ -43,4 +43,5 @@ public class PropertiesParserTest {
 //2- Not catching Exceptions like FileNotFound and IO
 //3- Since this is a util, it is better for it to be static
 //4- The RegEx was not being escaped properly so I added Pattern.quote(PATTERN) which returns a literal string replacement
-
+//5- it is possible to use Spring and have the Properties file injected using the <util:properties> instead of using the line below:
+//   props.load(PropertiesParserTest.class.getClassLoader().getResourceAsStream("test.properties"));
